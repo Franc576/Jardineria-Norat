547624320957227876041b14622d71c1eae5bc5f
salfarot-cloud <salfarot@alumnes.santjosepobrer.es>
2026-05-28 11:20:29 +0200

    feat: agregar nombres y descripciones breves a la flora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './sections/hero/hero.component';
 import { AboutComponent } from './sections/about/about.component';
 import { ServicesComponent } from './sections/services/services.component';
 import { ContactComponent } from './sections/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FloraComponent } from './sections/flora/flora.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     HeaderComponent,
+    FooterComponent,
     HeroComponent,
     AboutComponent,
     ServicesComponent,
     ContactComponent,
-    FooterComponent
+    FloraComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
